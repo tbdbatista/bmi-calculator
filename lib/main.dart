e2e644d1672c142465ed54e941f03438766b97b6
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-04-14 18:11:14 -0300

    Main - implement routes - Thiago Batista

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
+import 'package:calculadora_bmi/calculator_routes.dart';
 import 'package:flutter/material.dart';
 
-import 'calculator_page.dart';
-
 void main() {
-  runApp(const IMCCalculator());
+  runApp(IMCCalculator(route: CalculatorRoutes()));
 }
 
 class IMCCalculator extends StatelessWidget {
-  const IMCCalculator({super.key});
-
+  const IMCCalculator({super.key, required this.route});
+  final CalculatorRoutes route;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculadora de IMC',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
-        scaffoldBackgroundColor: const Color(0xFF010E21),
-      ),
-      home: const BMICalculatorPage(),
-    );
+        title: 'Calculadora de IMC',
+        theme: ThemeData.dark().copyWith(
+          primaryColor: const Color(0xFF0A0E21),
+          scaffoldBackgroundColor: const Color(0xFF010E21),
+        ),
+        initialRoute: '/home',
+        routes: route.routes);
   }
 }
