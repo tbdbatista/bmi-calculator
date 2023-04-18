c5077b52ed93f1885ff7bee8a9283e61bc18abe4
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2023-04-18 13:52:00 -0300

    ResultPage - update UI components - Thiago Batista

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,3 +1,4 @@
+import 'package:calculadora_bmi/calculator_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,51 @@ class ResultPage extends StatelessWidget {
         backgroundColor: MyColors.primaryColor,
         title: const Text('Calculadora IMC'),
       ),
-      body: const Text('Result Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 32.0, left: 8),
+              child: Text(
+                'Resultado',
+                style: MyStiles.biggerStyle,
+              ),
+            ),
+            Expanded(
+              child: CalculatorContainer(
+                childContainer: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'NORMAL',
+                      style: TextStyle(
+                        color: MyColors.contrastColor,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '22.1',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Here comes the sun',
+                      style: MyStiles.mainStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
